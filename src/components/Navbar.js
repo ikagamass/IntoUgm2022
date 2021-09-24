140da69e7b67601b3ee3e11390f6de42d19c9197
anwark7 <anwarkhairuddin11@gmail.com>
2021-09-24 17:18:08 +0700

    Configure Tailwind

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <div className="p-10 bg-gray-600">
+                <h1 className="text-white">alskdj</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
