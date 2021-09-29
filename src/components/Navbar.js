81e0cd8e5ace5daf7abb876ffaf798d99bdf1d30
anwark7 <anwarkhairuddin11@gmail.com>
2021-09-30 02:51:00 +0700

    Add temporary navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div>
-            <div className="p-10 bg-gray-600">
-                <h1 className="text-white">alskdj</h1>
+        <div className="flex-cc col h-screen p-6">
+            <div className="flex-cc w-1/2 h-1/3 my-12 rounded-xl border-4 border-red-600">
+                <h1 className="font-bold text-lg">Try Out</h1>
+            </div>
+            <div className="flex-cc w-1/2 h-1/3 my-12 rounded-xl border-4 border-red-600">
+                <h1 className="font-bold text-lg">Live</h1>
+            </div>
+            <div className="flex-cc w-1/2 h-1/3 my-12 rounded-xl border-4 border-red-600">
+                <h1 className="font-bold text-lg">Tour de Faculty</h1>
             </div>
         </div>
     )
