1e9b03b0dc134845eaf9f960db2638a48d0cca93
anwark7 <anwarkhairuddin11@gmail.com>
2021-09-30 02:50:40 +0700

    Configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    container: {
+      padding: {
+        DEFAULT: '5%',
+        sm: '32px',
+      },
+    },
+    extend: {
+      colors: {
+        base: '#ffffff',
+        primary: {
+          DEFAULT: '#3b3b3b',
+          red: '#FC3C5B'
+        }
+      },
+      fontFamily: {
+        main: '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
+      },
+      screens: {
+        '-2xl': { raw: '(max-width: 1535px)' },
+        '-xl': { raw: '(max-width: 1279px)' },
+        '-lg': { raw: '(max-width: 1023px)' },
+        '-md': { raw: '(max-width: 767px)' },
+        '-sm': { raw: '(max-width: 639px)' },
+      },
+    },
   },
   variants: {
     extend: {},
