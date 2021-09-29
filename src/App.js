190643dfe4f2db7b8449ee70f9a68bdbca3c835d
anwark7 <anwarkhairuddin11@gmail.com>
2021-09-30 03:04:54 +0700

    Install react-router-dom and add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <Navbar/>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/try-out" component={TryOut} />
+        <Route exact path="/live" component={Live} />
+        <Route exact path="/tour-de-faculty" component={TourDeFaculty} />
+      </Switch>
+    </Router>
   );
 }
 
