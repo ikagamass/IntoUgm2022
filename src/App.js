eb74837b7512603817f2ea09189df1e33f767782
anwark7 <anwarkhairuddin11@gmail.com>
2021-09-30 03:06:05 +0700

    Create pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Live from "./pages/Live";
+import TourDeFaculty from "./pages/TourDeFaculty";
+import TryOut from "./pages/TryOut";
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/try-out" component={TryOut} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
         <div className="flex-cc col h-screen p-6">
-            <div className="flex-cc w-1/2 h-1/3 my-12 rounded-xl border-4 border-red-600">
+            <Link to='try-out' className="flex-cc w-1/2 h-1/3 my-12 rounded-xl border-4 border-red-600">
                 <h1 className="font-bold text-lg">Try Out</h1>
-            </div>
-            <div className="flex-cc w-1/2 h-1/3 my-12 rounded-xl border-4 border-red-600">
+            </Link>
+            <Link to='live' className="flex-cc w-1/2 h-1/3 my-12 rounded-xl border-4 border-red-600">
                 <h1 className="font-bold text-lg">Live</h1>
-            </div>
-            <div className="flex-cc w-1/2 h-1/3 my-12 rounded-xl border-4 border-red-600">
+            </Link>
+            <Link to='tour-de-faculty' className="flex-cc w-1/2 h-1/3 my-12 rounded-xl border-4 border-red-600">
                 <h1 className="font-bold text-lg">Tour de Faculty</h1>
-            </div>
+            </Link>
         </div>
     )
 }
