756854b77b77c5cfb5bdb709ac80e6b6149b66ed
Rzkykhrllh <Rizkykirigaya@gmail.com>
2021-11-20 21:25:19 +0800

    responsive splashscreen

--- src/views/parts/SplashScreen.jsx
+++ src/views/parts/SplashScreen.jsx
@@ -8,9 +8,17 @@ function SplashScreen() {
       className="fixed z-50 flex w-screen min-h-screen bg-opacity-80 "
       style={{ backgroundColor: "#00295DDC" }}
     >
-      <div className="flex justify-center mx-auto my-auto gap-x-10">
-        <img src="/images/splashInto.png" alt="" className=" lg:w-80" />
-        <img src="/images/splashOrmada.png" alt="" className=" lg:w-80" />
+      <div className="flex flex-col justify-center gap-10 mx-auto my-auto sm:flex-row">
+        <img
+          src="/images/splashInto.png"
+          alt=""
+          className="w-36 lg:w-80 sm:w-56"
+        />
+        <img
+          src="/images/splashOrmada.png"
+          alt=""
+          className="w-36 lg:w-80 sm:w-56"
+        />
       </div>
     </div>
   );
