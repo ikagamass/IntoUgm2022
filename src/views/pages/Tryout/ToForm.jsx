98608896f21fa827ae9735ee88e4889815a68314
Airu <mr.khairullah@inovasi360.com>
2022-01-07 10:48:09 +0800

    Fix nomor telepohone

--- src/views/pages/Tryout/ToForm.jsx
+++ src/views/pages/Tryout/ToForm.jsx
@@ -63,7 +63,7 @@ function ToForm({ currentTitle, handleClick, hargaDibayar, mataUjian }) {
   }
 
   function validatePhoneNumber(input_str) {
-    var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,8}$/im;
 
     return re.test(input_str);
   }
